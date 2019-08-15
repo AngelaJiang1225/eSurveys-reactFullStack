6ef1912e8a5f2c566af03ac28e1c5ddff93172c0
AngelaJiang1225 <40841563+AngelaJiang1225@users.noreply.github.com>
2019-08-15 06:48:49 -0700

    add config

---
+++ server/config/dev.js
@@ -0,0 +1,11 @@
+// dont commit this
+module.exports = {
+  googleClientID:
+    "898657791685-jje5jdk52v2kco0nsqh93dpoibc7hdge.apps.googleusercontent.com",
+  googleClientSecret: "qiDOae1hfN9ETLnzlw8DcZ6F",
+  mongoURI:
+    "mongodb+srv://angela:UDLG1wLZ6dBnZqtF@emailycluster-lnlgh.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "hihihgg" //randomly selected
+};
+// heroku user: myuser code: dpPBm8Ksj6Y4aNZC
+//application code:mongodb+srv://myuser:dpPBm8Ksj6Y4aNZC@cluster0-lastn.mongodb.net/test?retryWrites=true&w=majority
---
+++ server/config/prod.js
@@ -0,0 +1,10 @@
+// prod.js - production keys here
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY //randomly selected
+  //googleRedirectURI: "https://glacial-hamlet-41120.herokuapp.com/"
+};
+//googleClientID:192838649253-nl03n3cg6ubfmpvs5cdcqtktjc8kg99t.apps.googleusercontent.com
+//4dAPGxA4pKEESnTXDTjOLpld
